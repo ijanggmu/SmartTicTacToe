6ca09accb10f457e8afdf170f86eaae9fc821feb
ijanggmu <sthaejan00@gmail.com>
2022-12-01 09:50:29 +0545

    :bulb: Added Comment

--- Forms/PlayersNameForm.cs
+++ Forms/PlayersNameForm.cs
@@ -1,4 +1,5 @@
 ﻿using SmartTicTacToe.Utility;
+using SmartTicTacToe.Utility.Helper;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,35 +15,79 @@ namespace SmartTicTacToe
 {
     public partial class PlayersNameForm : Form
     {
+        // When Player Click Player vs Player 
         public PlayersNameForm()
         {
             InitializeComponent();
         }
+        
+        // When Player Click Player vs RandomBot / Guided Bot
         public PlayersNameForm(string bot)
         {
             InitializeComponent();
-            if (bot == (Bot.RandomBot).ToString())
+            if (bot == nameof(Bot.RandomBot))
             {
-                playerTwoTextBox.Text = "RandomBot";
+                playerTwoTextBox.Text = nameof(Bot.RandomBot);
                 playerTwoTextBox.Enabled = false;
             }
-            else if (bot ==Bot.GuidedBot.ToString())
+            else if (bot == nameof(Bot.GuidedBot))
             {
-                playerTwoTextBox.Text = "GuidedBot";
+                playerTwoTextBox.Text = nameof(Bot.GuidedBot);
                 playerTwoTextBox.Enabled = false;
             }
-            
-            
+            PlayPlayerTwoFirst.Text = "Play Bot First";
+
+
         }
-        private void playTwoPlayer_Click(object sender, EventArgs e)
+        private void PlayClick(object sender, EventArgs e)
+        {
+            //Validation For Empty Player Name
+
+            if (String.IsNullOrEmpty(playerOneTextBox.Text) || String.IsNullOrEmpty(playerTwoTextBox.Text))
+            {
+                MessageBox.Show(ErrorMessage.EmptyPlayersNameError);
+            }
+            else if (!(String.IsNullOrEmpty(playerOneTextBox.Text)) && playerTwoTextBox.Text == nameof(Bot.GuidedBot))
+            {
+                TicTacToeMainGameForm.SetPlayerName(playerOneTextBox.Text, playerTwoTextBox.Text);
+                TicTacToeMainGameForm.botFirst = false;
+                // Choose the Guided Bot as  Opponent 
+                TicTacToeMainGameForm.SetOpponentChoice(playerTwoTextBox.Text);
+                TicTacToeMainGameForm mainGame = new TicTacToeMainGameForm();
+                mainGame.ShowDialog();
+                this.Close();
+            }
+            else if (!(String.IsNullOrEmpty(playerOneTextBox.Text)) && playerTwoTextBox.Text == nameof(Bot.RandomBot))
+            {
+                TicTacToeMainGameForm.SetPlayerName(playerOneTextBox.Text, playerTwoTextBox.Text);
+                TicTacToeMainGameForm.botFirst = false;
+                // Choose the Random Bot as  Opponent 
+                TicTacToeMainGameForm.SetOpponentChoice(playerTwoTextBox.Text);
+                TicTacToeMainGameForm mainGame = new TicTacToeMainGameForm();
+                mainGame.ShowDialog();
+                this.Close();
+            }
+            else
+            {
+                TicTacToeMainGameForm.SetPlayerName(playerOneTextBox.Text, playerTwoTextBox.Text);
+                TicTacToeMainGameForm mainGame = new TicTacToeMainGameForm();
+                mainGame.ShowDialog();
+                this.Close();
+
+            }
+        }
+
+        //When Player two first or Bot First is Clicked
+        private void PlayPlayerTwoFirstClick(object sender, EventArgs e)
         {
             if (String.IsNullOrEmpty(playerOneTextBox.Text) || String.IsNullOrEmpty(playerTwoTextBox.Text))
             {
-                MessageBox.Show("Player Name Cannot be Empty");
+                MessageBox.Show(ErrorMessage.EmptyPlayersNameError);
             }
             else if (!(String.IsNullOrEmpty(playerOneTextBox.Text)) && playerTwoTextBox.Text == Bot.GuidedBot.ToString())
             {
                 TicTacToeMainGameForm.SetPlayerName(playerOneTextBox.Text, playerTwoTextBox.Text);
+                TicTacToeMainGameForm.botFirst = true;
                 TicTacToeMainGameForm mainGame = new TicTacToeMainGameForm();
                 TicTacToeMainGameForm.SetOpponentChoice(playerTwoTextBox.Text);
                 mainGame.ShowDialog();
@@ -51,6 +96,7 @@ namespace SmartTicTacToe
             else if (!(String.IsNullOrEmpty(playerOneTextBox.Text)) && playerTwoTextBox.Text == Bot.RandomBot.ToString())
             {
                 TicTacToeMainGameForm.SetPlayerName(playerOneTextBox.Text, playerTwoTextBox.Text);
+                TicTacToeMainGameForm.botFirst = true;
                 TicTacToeMainGameForm.SetOpponentChoice(playerTwoTextBox.Text);
                 TicTacToeMainGameForm mainGame = new TicTacToeMainGameForm();
                 mainGame.ShowDialog();
@@ -58,18 +104,13 @@ namespace SmartTicTacToe
             }
             else
             {
-
                 TicTacToeMainGameForm.SetPlayerName(playerOneTextBox.Text, playerTwoTextBox.Text);
                 TicTacToeMainGameForm mainGame = new TicTacToeMainGameForm();
+                TicTacToeMainGameForm.SetPlayerTwoFirst();
                 mainGame.ShowDialog();
                 this.Close();
 
             }
-        }
-
-        private void playerTwoFirst_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
--- Utility/Helper/ErrorMessage.cs
+++ Utility/Helper/ErrorMessage.cs
@@ -3,5 +3,6 @@
     public static class ErrorMessage
     {
         public const string IntParseError = "Can't Parse The String To Int";
+        public const string EmptyPlayersNameError = "Player Name Cannot be Empty";
     }
 }
